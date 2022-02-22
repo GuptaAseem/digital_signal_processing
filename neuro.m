94c89b73a24fc498c67efaee3ff3ad9ccd160393
GuptaAseem <58130504+GuptaAseem@users.noreply.github.com>
2022-02-22 11:55:54 +0530

    minor changes and doubts

--- neuro.m
+++ neuro.m
@@ -1,11 +1,12 @@
 clear;
 
-%% Calculate ApEn matrix
+%% Setup
 data_dir = dir(pwd);
 % pwd folder should be 'F:\Aseem_IITD\ELL319_TP\data'
 n = 1098; % num of datapoints
-apEn = zeros(1098, 160);
 
+%% Calculate the ApEn matrix (use only once to generate file)
+apEn = zeros(1098, 160);
 for k = 4:length(data_dir)
     sprintf("working on index %d", k-3)
     sub_dir = data_dir(k).name;
@@ -20,9 +21,17 @@ for k = 4:length(data_dir)
         apEn(k-3, m) = approximateEntropy(data(:, m));
     end
 end
+save('..\apEn.mat', 'apEn')
 
+%% Directly read the ApEn matrix created above
+apEn_str = load('..\apEn.mat');
+apEn = apEn_str.apEn;
 %% Read the age matrix separately 
 age = readmatrix('..\age_final.csv');
+
+%% Combine age and ApEn for clustering data
+% age to be included or not????
+kmean_data = [apEn transpose(age)];
 
 %% Find optimum cluster number
 % eval = evalclusters(apEn, 'kmeans', 'silhouette', 'KList', 1:6);
@@ -30,35 +39,38 @@ age = readmatrix('..\age_final.csv');
 
 % below copied from -- https://stackoverflow.com/questions/46473719/optimum-number-of-clusters-in-k-mean-clustering-using-bic-matlab
 BIC = []; % Bayesian Information Criterion 
-for k=1:10  % number of clusters
+for k=1:9  % number of clusters
     RSS=0;  % residual sum of squares
-    [idx, C] = kmeans(apEn, k); 
+    [idx, C] = kmeans(kmean_data, k); 
     
     for i = 1:n
-        RSS = RSS + sqrt((apEn(i,1) - C(idx(i),1))^2 + (apEn(i,2) - C(idx(i),2))^2);
+        RSS = RSS + sqrt((kmean_data(i,1) - C(idx(i),1))^2 + (kmean_data(i,2) - C(idx(i),2))^2);
     end
     BIC(k) = n*log(RSS/n)+(k*3)*log(n);
 end
 [min_BIC, k_opt] = min(BIC);
 plot(BIC)
 % above gives 7 but should be 4, needs minor corrections
+% after age answer becomes even more incorrect
 
 %% Perform kmeans on k_opt
-idx = kmeans(apEn, k_opt);
+idx = kmeans(kmean_data, k_opt);
 
 %% to find age groups from optimal clusters
-lower = zeros(k_opt);
+lower = Inf(k_opt);
 upper = zeros(k_opt);
 
 for k = 1:n
     if age(k) > upper(idx(k))
-        upper(idx(k)) = age(k)
+        upper(idx(k)) = age(k);
     end
     if age(k) < lower(idx(k))
-        lower(idx(k)) = age(k)
+        lower(idx(k)) = age(k);
     end
 end
 
 for k = 1:k_opt
     sprintf("age boundary %d: %f to %f", k, lower(k), upper(k))
-end  
+end
+
+%% 
